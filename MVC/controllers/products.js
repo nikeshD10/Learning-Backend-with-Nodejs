0b8597db529fb6c5afd590f22892f64bd0272edb
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-06 17:55:30 +0100

    Created MVC structure and added product model to store product in array

---
+++ MVC/controllers/products.js
@@ -0,0 +1,34 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  // products is an array of products
+  // we are calling the fetchAll method on the Product class
+  // this method returns the products array
+  // we are storing the array in the products variable
+  const products = Product.fetchAll();
+  // we are rendering the shop view
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
---
+++ MVC/models/product.js
@@ -0,0 +1,19 @@
+const products = [];
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    // we are pushing the product object into the products array
+    // this represents the current object
+    products.push(this);
+  }
+
+  // static means we can call this method directly on the class itself
+  // and not on an instantiated object
+  static fetchAll() {
+    return products;
+  }
+};
