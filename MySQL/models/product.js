92c1f9631be7e694bd9972b6c0f5d4f800b28112
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-08 03:56:55 +0100

    To connect the mysql with our app installed mysql

---
+++ MySQL/models/product.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+const Cart = require("./cart");
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      // if we have an existing product
+      if (this.id) {
+        // we need to find the index of the product we want to update
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        // we need to copy the existing product
+        const updatedProducts = [...products];
+        // we need to update the product
+        updatedProducts[existingProductIndex] = this;
+        // we need to write the updated product to the file
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        // if we don't have an existing product
+        // we need to add a new product
+        // we need to set the id of the new product
+        // we need to write the new product to the file
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      // we need to find the product we want to delete
+      const product = products.find((p) => p.id === id);
+      // we need to filter the products and remove the product we want to delete
+      const updatedProducts = products.filter((p) => p.id !== id);
+      // we need to write the updated products to the file
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          // if we don't have an error then we need to delete the product from the cart
+          Cart.deleteProduct(id, product.price);
+        }
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
+  }
+};
