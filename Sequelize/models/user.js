ab7a58f7e4b30329c9987dda232e4a1198382572
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-09 02:51:42 +0100

    Created user model

---
+++ Sequelize/models/user.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../util/database");
+
+const User = sequelize.define("user", {
+  id: {
+    // type is defined by Sequelize Object
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+});
