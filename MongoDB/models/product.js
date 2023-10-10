3351e558d893d97181c5de0c9b0ee201ae2210d8
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-11 00:34:33 +0100

    Reimplemented update product functionality using mongodb

--- MongoDB/models/product.js
+++ MongoDB/models/product.js
@@ -2,19 +2,30 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id ? new mongodb.ObjectId(id) : null; // if id is not null then create a new object id
   }
 
   save() {
-    // to save a product we need to connect to t he database
+    // to save a product we need to connect to the database
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // update the product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+      // updateOne takes 2 arguments: the first is the filter i.e which document we want to update, the second is the update
+      // $set is a special operator that tells mongodb what to update
+    } else {
+      // insert the product
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
       })
@@ -46,7 +57,6 @@ class Product {
       .next() // next returns a promise which is a product
       .then((product) => {
         // then returns a product
-        console.log(product);
         return product;
       })
       .catch((err) => {
