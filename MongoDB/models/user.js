22632881f778ee9f024c7ce5e4b2eff537489534
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-11 03:10:18 +0100

    Added the storing multiple products in the cart functionality

--- MongoDB/models/user.js
+++ MongoDB/models/user.js
@@ -4,7 +4,7 @@ class User {
   constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
-    this.cart = cart; // {items: []}
+    this.cart = cart; // {items: [{productId: 123, quantity: 2}]}
     this._id = id;
   }
 
@@ -13,50 +13,34 @@ class User {
     return db.collection("users").insertOne(this);
   }
 
-  // addToCart(product) {
-  //   const db = getDb();
-  //   // check if the product already exists in the cart
-  //   const cartProductIndex = this.cart.items.findIndex((cp) => {
-  //     return cp._id.toString() === product._id.toString();
-  //   });
-  //   // copy the cart items
-  //   const updatedCartItems = [...this.cart.items];
-  //   let newQuantity = 1;
-  //   // if the product already exists in the cart
-  //   if (cartProductIndex >= 0) {
-  //     // update the quantity
-  //     newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-  //     // update the cart items
-  //     updatedCartItems[cartProductIndex].quantity = newQuantity;
-  //   } else {
-  //     // if the product doesn't exist in the cart
-  //     // add the product to the cart
-  //     updatedCartItems.push({
-  //       productId: new mongodb.ObjectId(product._id),
-  //       quantity: newQuantity,
-  //     });
-  //   }
-  //   // copy the cart
-  //   const updatedCart = {
-  //     items: updatedCartItems,
-  //   };
-  //   // update the cart
-  //   return db
-  //     .collection("users")
-  //     .updateOne(
-  //       { _id: new mongodb.ObjectId(this._id) },
-  //       { $set: { cart: updatedCart } }
-  //     );
-  // }
-
   addToCart(product) {
-    const updatedCart = {
-      items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }],
-    };
-
-    // const updatedCart = { items: [{ ...product, quantity: 1 }] };
-
     const db = getDb();
+    // check if the product already exists in the cart
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    // copy the cart items
+    const updatedCartItems = [...this.cart.items];
+    let newQuantity = 1;
+    // if the product already exists in the cart
+    if (cartProductIndex >= 0) {
+      // get previous quantity and increment by 1
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      // update the cart items quantity
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      // if the product doesn't exist in the cart
+      // add the product to the cart
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    // copy the cart
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    // update the cart
     return db
       .collection("users")
       .updateOne(
@@ -64,6 +48,22 @@ class User {
         { $set: { cart: updatedCart } }
       );
   }
+
+  // addToCart(product) {
+  //   const updatedCart = {
+  //     items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }],
+  //   };
+
+  //   // const updatedCart = { items: [{ ...product, quantity: 1 }] };
+
+  //   const db = getDb();
+  //   return db
+  //     .collection("users")
+  //     .updateOne(
+  //       { _id: new mongodb.ObjectId(this._id) },
+  //       { $set: { cart: updatedCart } }
+  //     );
+  // }
 
   static findById(userId) {
     const db = getDb();
