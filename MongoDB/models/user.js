442d1ea2307e239db5ffc2b48f535f4d961d0ccd
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-11 03:51:21 +0100

    Get and create order feature implemented

--- MongoDB/models/user.js
+++ MongoDB/models/user.js
@@ -92,6 +92,43 @@ class User {
       });
   }
 
+  addOrder() {
+    const db = getDb();
+    // get the cart
+    return this.getCart()
+      .then((products) => {
+        // create the order
+        const order = {
+          items: products,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        // insert the order
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        // reset the cart
+        this.cart = { items: [] };
+        // update the cart
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new mongodb.ObjectId(this._id) })
+      .toArray();
+  }
+
   static findById(userId) {
     const db = getDb();
     return (
