165263fc3393d967831fe354ac43d45611deb01c
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-13 17:25:05 +0100

    Implemented the authentication flow

--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -1,39 +1,71 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
   });
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
+  User.findById("5bab316ce0a7c75f783cb8a8")
+    .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save(err => {
+      req.session.save((err) => {
         console.log(err);
-        res.redirect('/');
+        res.redirect("/");
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  // Now validate the email and password
+  // We will use the express-validator package for this
+  // We will use the check() method to check the email and password
+  // The check() method takes the field name as the first argument
+  // The second argument is the error message that we want to show if the validation fails
+  // We can chain multiple validation methods to the check() method
+  // The isEmail() method checks if the field is a valid email address
+  // The isLength() method checks if the field has a minimum length
+
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
 };
