31f83015b40fe75c0515bbf9d1193ca55d1a523c
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-13 01:10:09 +0100

    Used the session middleware

--- SessionsAndCookies/app.js
+++ SessionsAndCookies/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -18,7 +19,18 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "MySecret",
+    resave: false, // This means that the session will not be saved on every request that is done to the server but only if something changed in the session object
+    saveUninitialized: false, // This ensures that no session gets saved for a request where it doesn't need to be saved
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+  })
+);
 
+// Note: this middleware is executed for every incoming request
+// Because this middleware is executed for every incoming request, it's a good place to check if the user is logged in
+// This middleware runs in every incoming requests before our routes handles them
 app.use((req, res, next) => {
   User.findById("5bab316ce0a7c75f783cb8a8")
     .then((user) => {
--- SessionsAndCookies/controllers/auth.js
+++ SessionsAndCookies/controllers/auth.js
@@ -1,17 +1,27 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+  // cookies are stored in the client side
+  // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+  // const isLoggedIn = req.session.isLoggedIn;
+  console.log(req.session.isLoggedIn);
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
-
 exports.postLogin = (req, res, next) => {
   // this data is lost after the response is sent so we need to use sessions
   // Note : redirection creates brand new request
   // req.isLoggedIn = true;
+  // res.setHeader("Set-Cookie", "loggedIn=true", HttpOnly);
 
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  // Note:
+  // 1. We can't store objects in sessions
+  // 2. Sessions are stored in the server side
+  // 2. By default sessions is stored in memory
+  // 3. Sessions are stored in a cookie in the client side
+  // This session object is added by session middleware
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
