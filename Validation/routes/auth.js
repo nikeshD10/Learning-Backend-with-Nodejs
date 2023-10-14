d066aff4a8ab75149fc73d15e4e5369b0e99b278
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-14 19:27:51 +0100

    Added sanitizor

--- Validation/routes/auth.js
+++ Validation/routes/auth.js
@@ -24,10 +24,14 @@ router.get("/signup", authController.getSignup);
 router.post(
   "/login",
   [
-    body("email").isEmail().withMessage("Please enter a valid email address."),
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email address.")
+      .normalizeEmail(), // Built in sanitizor
     body("password", "Password has to be valid.")
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(), // Built in sanitizor
   ],
   authController.postLogin
 );
@@ -38,12 +42,12 @@ router.post(
     check("email")
       .isEmail()
       .withMessage("Please enter the valid email!")
+
       .custom((value, { req }) => {
         // if (value.split("@")[1] !== "gmail.com") {
         //   throw new Error("This email address is forbidden.");
         // }
         // return true;
-
         User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
             return Promise.reject(
@@ -51,20 +55,24 @@ router.post(
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
 
     body(
       "password",
       "Please enter a password with only numbers and text and at least 5 characters." // This is a custom error message. so we don't repeat withMessage on different error message or on different validators
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body("confirmPassword").custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Password have to match!");
-      }
-      return true;
-    }),
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Password have to match!");
+        }
+        return true;
+      }),
   ],
   authController.postSignup
 );
