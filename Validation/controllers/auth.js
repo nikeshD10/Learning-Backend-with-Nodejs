1dcf1266f14dc97f024b16e7198b5490a372c2b6
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-10-14 18:49:30 +0100

    Added email, password and confirm password validation for sign up

--- Validation/controllers/auth.js
+++ Validation/controllers/auth.js
@@ -80,48 +80,50 @@ exports.postSignup = (req, res, next) => {
   const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).render("auth/signup", {
       path: "/signup",
       pageTitle: "Signup",
-      errorMessage: errors.array(),
+      errorMessage: errors.array()[0].msg,
     });
   }
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          "error",
-          "E-Mail exists already, please pick a different one."
-        );
-        return res.redirect("/signup");
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/login");
-          return transporter.sendMail({
-            to: email,
-            from: "shop@node-complete.com",
-            subject: "Signup succeeded!",
-            html: "<h1>You successfully signed up!</h1>",
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+
+  // ---------------  Because now we are validating on routes so we don't need it here -------------
+  // User.findOne({ email: email })
+  //   .then((userDoc) => {
+  //     if (userDoc) {
+  //       req.flash(
+  //         "error",
+  //         "E-Mail exists already, please pick a different one."
+  //       );
+  //       return res.redirect("/signup");
+  //     }
+  // return bcrypt
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+      return transporter.sendMail({
+        to: email,
+        from: "shop@node-complete.com",
+        subject: "Signup succeeded!",
+        html: "<h1>You successfully signed up!</h1>",
+      });
     })
     .catch((err) => {
       console.log(err);
     });
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
 };
 
 exports.postLogout = (req, res, next) => {
--- Validation/routes/auth.js
+++ Validation/routes/auth.js
@@ -1,26 +1,73 @@
-const express = require('express');
-const { check } = require('express-validator/check');
+const express = require("express");
+// Typically we want to validate on post requests, not get requests.
+// This is because get requests are used to fetch data, not to change data.
+// We can use the check() method to validate a field.
+// We can chain multiple checks together.
+// We can use the isEmail() method to validate that the email is valid.
+// We can use the isLength() method to validate that the password is a certain length.
+// We can use the trim() method to remove whitespace from the beginning and end of the input.
+// We can use the normalizeEmail() method to normalize the email address.
+// We can use the withMessage() method to add a custom error message.
+// We can use the custom() method to add a custom validation function.
+const { check, body } = require("express-validator/check");
 
-const authController = require('../controllers/auth');
+const User = require("../models/user");
+
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter the valid email!")
+      .custom((value, { req }) => {
+        // if (value.split("@")[1] !== "gmail.com") {
+        //   throw new Error("This email address is forbidden.");
+        // }
+        // return true;
 
-router.post('/logout', authController.postLogout);
+        User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "Email exists already, please pick a different one."
+            );
+          }
+        
+      }),
 
-router.get('/reset', authController.getReset);
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters." // This is a custom error message. so we don't repeat withMessage on different error message or on different validators
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password have to match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
-router.post('/reset', authController.postReset);
+router.post("/logout", authController.postLogout);
 
-router.get('/reset/:token', authController.getNewPassword);
+router.get("/reset", authController.getReset);
 
-router.post('/new-password', authController.postNewPassword);
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
